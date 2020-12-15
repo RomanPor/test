21713e2b518b3b4d357255552b3d45ffbec27789
romium <39771039+RomanPor@users.noreply.github.com>
2020-12-15 15:41:04 +0300

    Add files via upload

---
+++ precache-manifest.dc0d61328cdd22599a53ee168d2e4932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21929c1a4a724bd22ef464cd7f9f0eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e0c8830134ea918c7d",
+    "url": "/static/css/main.86f2f96f.chunk.css"
+  },
+  {
+    "revision": "3ea5ae1f83ef1e294362",
+    "url": "/static/js/2.60e46912.chunk.js"
+  },
+  {
+    "revision": "05a0d35bfda693f65fb268130209c048",
+    "url": "/static/js/2.60e46912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e0c8830134ea918c7d",
+    "url": "/static/js/main.9a9d7d6b.chunk.js"
+  },
+  {
+    "revision": "d8525f03fa4f1f964a93",
+    "url": "/static/js/runtime-main.456f2914.js"
+  }
+]);
